66385db43189623e9eaf5d4c15bb83dc49decc95
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-05-22 18:54:54 -0400

    #8, stripe set up.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import './index.scss';
+import { Elements } from '@stripe/react-stripe-js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
+import './index.scss';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +17,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
