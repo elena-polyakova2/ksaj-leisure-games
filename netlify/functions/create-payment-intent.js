68822490079a5de73176a09cbe61b04cb86ea413
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-05-29 21:50:45 -0400

    #10. Serverless function set up.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+//function that receives request to expect amount value to pass into stripe to make payment
+exports.handler = async (event) => {
+  try {
+    //make payment intent using stripe
+    const { amount } = JSON.parse(event.body);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "cad",
+      payment_method_types: ["card"]
+    });
+    //when done succesfully
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent })
+    }
+
+  } catch (error) {
+    console.log({ error });
+
+    return {
+      staus: 400,
+      body: JSON.stringify({ error })
+    }
+  }
+};
--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,12 +1,25 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import { PaymentFormContainer, FormContainer } from './payment-form.styles';
 
 
 const PaymentForm = () => {
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const paymentHangler = async (e) => {
+  e.preventDefault();
+
+  if(!stripe || !elements) {
+    return;
+  }
+}
+
   return(
     <PaymentFormContainer>
       <FormContainer>
+        <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
       </FormContainer>
