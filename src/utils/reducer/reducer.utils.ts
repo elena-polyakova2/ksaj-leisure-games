7a1b71b99c177552af6d62ce41b9d0cad6393787
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-06-11 15:12:32 -0400

    #17,Predicate functions are typed. Intersections, retutn types and withMatcher typed.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,5 +1,28 @@
 import { AnyAction } from 'redux';
 
+//extend action creators to perform type check to respond approprietly to the actions
+type Matchable<AC extends () => AnyAction> = AC & {
+  type: ReturnType<AC>['type'];
+  match(action: AnyAction): action is ReturnType<AC>;
+};
+
+//overload function with types of creators that will be received
+export function withMatcher<AC extends () => AnyAction & { type: string }>(actionCreator: AC): Matchable<AC>;
+
+//funvtion to receive any number of arguments in array
+export function withMatcher<AC extends (...args: any[]) => AnyAction & { type: string }>(actionCreator: AC): Matchable<AC>;
+
+//actual function withMatcher
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type;
+  //mappable object
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    }
+  })
+};
 
 export type ActionWithPayload<T, P> = {
   type: T;
