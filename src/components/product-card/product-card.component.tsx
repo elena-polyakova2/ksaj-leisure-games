567c5ae77d84c323b5bdfa57eca7e7a65d0edf92
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-07-01 22:50:46 -0400

    product-card component typed.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,14 +1,21 @@
+import { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectCartItems } from '../../store/cart/cart.selector';
 import { addItemToCart } from '../../store/cart/cart.action';
 import { ProductCartContainer, Footer, Name, Price } from './product-card.styles';
+import { CategoryItem } from '../../store/categories/categories.types';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
-const ProductCard = ({ product }) => {
+type ProductCardProps = {
+  product: CategoryItem;
+};
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
   
 const { name, price, imageUrl } = product;
 const dispatch = useDispatch();
 const cartItems = useSelector(selectCartItems);
+
 const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
 
 return (
