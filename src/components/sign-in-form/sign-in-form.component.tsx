90d173252331d2390d43e53ead46f1d51f2e1e32
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-07-07 23:09:01 -0400

    service-worker.js added.
    Co-authored-by:elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -5,6 +5,7 @@ import FormInput from "../form-input/form-input.component";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
 import { googleSignInStart, emailSignInStart } from "../../store/user/user.action";
+import { useNavigate } from "react-router-dom";
 
 const defaultFormFields = {
   email: '',
@@ -14,6 +15,7 @@ const defaultFormFields = {
 const SignInForm = () => {
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
@@ -23,6 +25,7 @@ const SignInForm = () => {
 
   const signInWithGoogle = async () => {
     dispatch(googleSignInStart());
+    navigate('/');
   };
 
   //make user documents
@@ -32,7 +35,9 @@ const SignInForm = () => {
     //create user
     try{
       dispatch(emailSignInStart(email, password));
+      navigate('/');
       resetFormFields();
+      
     } catch(error) {
       switch ((error as AuthError).code) {
         case AuthErrorCodes.INVALID_PASSWORD:
