d6c0247cdb4ffcee2cf12677abc1c89a593f4d05
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-07-16 23:42:10 -0400

    authentication.styles changed for a mobile screen.

--- src/routes/authentication/authentication.styles.jsx
+++ src/routes/authentication/authentication.styles.jsx
@@ -5,4 +5,14 @@ export const AuthenticationContainer = styled.div`
   width: 900px;
   justify-content: space-between;
   margin: 30px auto;
+
+  @media screen and (max-width: 800px) {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-row-gap: 25px;
+    align-items: center;
+    justify-content: center;
+    margin: 5px;
+    padding: 5px;
+  }
 `;
