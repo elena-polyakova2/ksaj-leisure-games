b4426be05d60e202d35ab7351c86b8af77b66a3c
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-07-16 23:46:45 -0400

    checkout.styles changed for a mobile screen.

--- src/routes/checkout/checkout.styles.jsx
+++ src/routes/checkout/checkout.styles.jsx
@@ -7,6 +7,12 @@ export const CheckoutContainer = styled.div`
   flex-direction: column;
   align-items: center;
   margin: 50px auto 0;
+
+  @media screen and (max-width: 800px) {
+    width: 90vw;
+    margin-top: 35px;
+    min-height: 50vh;
+  }
 `;
 
 export const CheckoutHeader = styled.div`
@@ -15,6 +21,10 @@ export const CheckoutHeader = styled.div`
   display: flex;
   justify-content: space-between;
   border-bottom: 1px solid darkgrey;
+
+  @media screen and (max-width: 800px)  {
+    width: 80vw;  
+  }
 `;
 
 export const HeaderBlock = styled.div`
@@ -23,6 +33,10 @@ export const HeaderBlock = styled.div`
 
   &:last-child {
     width: 8%;
+  }
+
+  @media screen and (max-width: 400px) {
+    font-size: 10px;
   }
 `;
 
