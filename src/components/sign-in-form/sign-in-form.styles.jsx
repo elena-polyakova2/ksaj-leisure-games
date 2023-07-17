0f3b896d9c376098fdffb4dccb8f5a471b74beb4
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-07-16 23:18:39 -0400

    sign-in-form.styles changed for a mobile screen.

--- src/components/sign-in-form/sign-in-form.styles.jsx
+++ src/components/sign-in-form/sign-in-form.styles.jsx
@@ -8,9 +8,23 @@ export const SignInContainer = styled.div`
   h2 {
     margin: 10px 0;
   }
+  
+  @media screen and (max-width: 800px) {
+  width: 80vw;
+  align-items: center;
+  justify-content: center;
+}
 `;
 
 export const ButtonsContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  
+  @media screen and (max-width: 800px) {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 15px; 
+}
 `;
+
+
